79e626978f144f3019fae76836b931f5b2267a5b
Iddan Gunawan <iddan.gunawan@tokodistributor.com>
2020-12-14 15:27:22 +0700

    Testing Use Case class

--- app/src/test/java/com/mylektop/awesomeapp/screens/curated/CuratedUseCaseTest.kt
+++ app/src/test/java/com/mylektop/awesomeapp/screens/curated/CuratedUseCaseTest.kt
@@ -1,14 +1,15 @@
-package com.mylektop.awesomeapp.repository
+package com.mylektop.awesomeapp.screens.curated
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.mylektop.awesomeapp.base.BaseUTTest
 import com.mylektop.awesomeapp.di.configureTestAppComponent
 import com.mylektop.awesomeapp.network.curated.CuratedAPIService
-import junit.framework.TestCase.assertEquals
-import junit.framework.TestCase.assertNotNull
+import com.mylektop.awesomeapp.repository.CuratedRepository
 import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.Before
+
+import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,10 +22,13 @@ import java.net.HttpURLConnection
  * Created by iddangunawan on 12/14/20
  */
 @RunWith(JUnit4::class)
-class CuratedRepositoryTest : BaseUTTest() {
+class CuratedUseCaseTest : BaseUTTest() {
 
     //Target
-    private lateinit var mRepo: CuratedRepository
+    private lateinit var mCuratedUseCase: CuratedUseCase
+
+    //Inject login repo created with koin
+    val mCuratedRepo: CuratedRepository by inject()
 
     //Inject api service created with koin
     val mAPIService: CuratedAPIService by inject()
@@ -48,11 +52,11 @@ class CuratedRepositoryTest : BaseUTTest() {
     }
 
     @Test
-    fun test_curated_repo_retrieves_expected_data() = runBlocking<Unit> {
+    fun test_curated_use_case_returns_expected_value() = runBlocking {
         mockNetworkResponseWithFileContent("success_list_response.json", HttpURLConnection.HTTP_OK)
-        mRepo = CuratedRepository()
+        mCuratedUseCase = CuratedUseCase()
 
-        val dataReceived = mRepo.getCuratedPhotoData(mParamPerPage, mParamPage)
+        val dataReceived = mCuratedUseCase.processCuratedUseCase(mParamPerPage, mParamPage)
 
         assertNotNull(dataReceived)
         assertEquals(dataReceived.total_results, mTotalResults)
